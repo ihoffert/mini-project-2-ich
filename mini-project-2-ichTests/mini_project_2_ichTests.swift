e4b2bdf9a40778b5266de5ca8eb2efcd4ba384a0
ingrid hoffert <ingihoff@gmail.com>
2021-07-15 10:23:05 -0700

    Initial Commit

---
+++ mini-project-2-ichTests/mini_project_2_ichTests.swift
@@ -0,0 +1,34 @@
+//
+//  mini_project_2_ichTests.swift
+//  mini-project-2-ichTests
+//
+//  Created by Ingrid Hoffert on 7/15/21.
+//  Copyright © 2021 Ingrid Hoffert. All rights reserved.
+//
+
+import XCTest
+@testable import mini_project_2_ich
+
+class mini_project_2_ichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
